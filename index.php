f95d74e19485df840bc22e62cd93b18b65bfcbc8
samin2005 <90718087+samin2005@users.noreply.github.com>
2021-11-25 10:26:51 -0800

    hdf

--- index.php
+++ index.php
@@ -1,30 +1,59 @@
 <?
-    ini_set('display_errors', 'Off');
- error_reporting(0);
-    session_start();
+ini_set('display_errors', 'Off');
+error_reporting(0);
+session_start();
 
-    $_SESSION['time'] = time();
+ if(time() - $_SESSION['timefour'] < 15 || !isset($_SESSION['timefour']))
+ {
+ 	echo "you did not complete the linkvertise";
+ 	// echo "<meta http-equiv='Refresh' Content='4;url=https://keyauth.com/snowwy/'>"; 
+ 	return;
+ }
 
-    header("Refresh: 5; URL=https://link-to.net/165411/cheatooo");
-    // print("Powered by KeyAuth.com login system");
+$ch = curl_init("https://keyauth.com/api/seller/?sellerkey=010c4da44ae7db416a21664ad2ed2f60&type=add&expiry=0.041666666666667&mask=XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX&level=1&amount=1&format=text");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+$key = curl_exec($ch);
+curl_close($ch);
+
+$_SESSION['time'] = NULL;
+$_SESSION['timetwo'] = NULL;
+$_SESSION['timethree'] = NULL;
+$_SESSION['timefour'] = NULL;
+// print("Powered by KeyAuth.com login system");
 ?>
-
 
 <html>
 
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
-<meta charset="utf-8">
-
-
+    <script>
+        function copyToClipboard() {
+            var e = "<?php echo $key ?>";
+            var tempItem = document.createElement('input');
+            tempItem.setAttribute('type','text');
+            tempItem.setAttribute('display','none');
+            let content = e;
+            if (e instanceof HTMLElement) {
+            		content = e.innerHTML;
+            }
+            tempItem.setAttribute('value',content);
+            document.body.appendChild(tempItem);
+            tempItem.select();
+            document.execCommand('Copy');
+            tempItem.parentElement.removeChild(tempItem);
+            alert("Successfully copied the key to your clipboard!");
+        }
+</script>
+    
 <title>KeySystem</title>
 <meta charset="utf-8" />
-<link rel="stylesheet" type="text/css" href="MainDesign.css"/>
+<link rel="stylesheet" type="text/css" href="MainDesign.css" />
 <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
-
 <div class="keysyscontainer">
     <div class="box">
-        <span class="title">TEAM JOY</span>
-        <span class="text"> You will have to go through four linkvertises. The key will last around 10 minutes. Redirecting in 5 seconds... </span>
-        <span class="text"> No one likes advertisments, but going through this supports the creators of KeySystem. </span>
+        <span class="title">GENARATE KEY</span>
+        <span class="text"> This key is valid for around 10 minutes only. </span>
+        <span class="text"> Your key: <?php echo $key ?></span>
+        <button onclick = "return copyToClipboard()" class="button Copy">Copy Key</button>
 </html>
